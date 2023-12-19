9f4733b8a33a7c506b7fe542c241018f9a6f656e
iko233 <iko@iko.ski>
2023-12-19 14:46:52 +0800

    Update cloudflare-woker.js
    1. 优化代码结构
    2. 支持多轮对话场景

--- cloudflare-woker.js
+++ cloudflare-woker.js
@@ -1,12 +1,12 @@
-const geminiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key='
-const AUTH_KEY_NO_PROVIDE_FAIL_MESSAGE = `{
-  "error": {
-      "code": null,
-      "message": "You didn't provide an API key. You need to provide your API key in an Authorization header using Bearer auth (i.e. Authorization: Bearer YOUR_KEY), or as the password field (with blank username) if you're accessing the API from your browser and are prompted for a username and password. You can obtain an API key from https://platform.openai.com/account/api-keys.",
-      "param": null,
-      "type": "invalid_request_error"
-  }
-}`
+const geminiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=';
+const AUTH_KEY_NO_PROVIDE_FAIL_MESSAGE = JSON.stringify({
+    "error": {
+        "code": null,
+        "message": "You didn't provide an API key. You need to provide your API key in an Authorization header using Bearer auth (i.e. Authorization: Bearer YOUR_KEY), or as the password field (with blank username) if you're accessing the API from your browser and are prompted for a username and password. You can obtain an API key from https://platform.openai.com/account/api-keys.",
+        "param": null,
+        "type": "invalid_request_error"
+    }
+});
 
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request));
@@ -19,64 +19,24 @@ async function handleRequest(request) {
     }
     let token = authorization.replace('Bearer ', '');
     let body = await request.json();
-    let message = body.messages[0].content;
+    let geminiTalkList = convertGptTalkList2GeminiTalkList(body.messages);
     let isStream = body.stream;
+    console.log("请求列表为:", geminiTalkList)
+    const geminiResponse = await requestGemini(token, geminiTalkList);
+    console.log("响应内容为:", geminiResponse)
+    const geminiText = getGeminiText(geminiResponse);
 
-    const response = await requestGemini(token, message);
-
-    if (isStream) {
-        const stream = new ReadableStream({
-            start(controller) {
-              controller.enqueue(`data: [DONE] \n\n`);
-              controller.enqueue(`data: {
-                  "id": "chatcmpl-9yBOQH3TdLCRmVS82gnABaHQ0jb25",
-                  "object": "chat.completion.chunk",
-                  "created": 1702820230,
-                  "model": "gpt-3.5-turbo",
-                  "choices": [
-                      {
-                          "delta": {},
-                          "index": 0,
-                          "finish_reason": "stop"
-                      }
-                  ]
-                  }\n\n`);
-              controller.enqueue("data: " + convert2GptResponse(isStream, getGeminiText(response)) + `\n\n`);
-              controller.close(); 
-            }
-        });
-        // 设置适当的响应头
-        const headers = new Headers();
-        headers.set('Content-Type', 'text/event-stream'); // 设置为text/event-stream
-
-        // 返回流式数据给客户端
-        return new Response(stream, {
-            status: 200,
-            statusText: 'OK',
-            headers,
-        });
-    } else {
-        return new Response(convert2GptResponse(isStream, getGeminiText(response)), {
-            headers: {
-                "content-type": "application/json",
-                "access-control-allow-credentials": "true",
-                "access-control-allow-headers": "Origin,Content-Type,Accept,User-Agent,Cookie,Authorization,X-Auth-Token,X-Requested-With",
-                "access-control-allow-methods": "GET,PUT,POST,DELETE,PATCH,HEAD,CONNECT,OPTIONS,TRACE",
-                "access-control-allow-origin": "*",
-                "access-control-max-age": "3628800",
-            }
-        }
-        );
+    if (!isStream) {
+        return buildResponse(geminiText);
     }
-
+    return buildStreamResponse(geminiText);
 }
 
-
-
-async function requestGemini(token, message) {
-    console.log("开始请求gemini:" + geminiUrl + token);
+async function requestGemini(token, talkList) {
+    const requestBody = JSON.stringify({ "contents": talkList });
+    console.log("开始请求gemini:" + geminiUrl + token + requestBody);
     const response = await fetch(geminiUrl + token, {
-        body: '{"contents": [{"parts": [{"text": "#{message}"}]}]}'.replace('#{message}', message),
+        body: requestBody,
         method: "POST",
         headers: {
             "content-type": "application/json;charset=UTF-8",
@@ -89,47 +49,93 @@ function getGeminiText(response) {
     return JSON.parse(response).candidates[0].content.parts[0].text;
 }
 
-function convert2GptResponse(isStream, msg) {
-    if (isStream) {
-        return `{
-      {
-        "id": "chatcmpl-9yBOQH3TdLCRmVS82gnABaHQ0jb25",
-        "object": "chat.completion.chunk",
-        "created": 1702820230,
-        "model": "gpt-3.5-turbo",
+function convert2GptResponse(msg) {
+    return JSON.stringify({
         "choices": [
             {
-                "delta": {
-                  "content": "#{response}"
-                },
+                "finish_reason": "stop",
                 "index": 0,
-                "finish_reason": "stop"
+                "message": {
+                    "content": msg,
+                    "role": "assistant"
+                }
             }
-        ]
-    }
-  }`.replace('#{response}', msg);
-    } else {
-        return `{
-      "choices": [
-          {
-              "finish_reason": "stop",
-              "index": 0,
-              "message": {
-                  "content": "#{response}",
-                  "role": "assistant"
-              }
-          }
-      ],
-      "created": 1702814742,
-      "id": "chatcmpl-1Qq8McZaJvX4WrWqJgUHc7gOageEG",
-      "model": "",
-      "object": "chat.completion",
-      "usage": {
-          "completion_tokens": 42,
-          "prompt_tokens": 0,
-          "total_tokens": 42
-      }
-  }`.replace('#{response}', msg);
-    }
+        ],
+        "created": 1702814742,
+        "id": "chatcmpl-1Qq8McZaJvX4WrWqJgUHc7gOageEG",
+        "model": "",
+        "object": "chat.completion",
+        "usage": {
+            "completion_tokens": 42,
+            "prompt_tokens": 0,
+            "total_tokens": 42
+        }
+    })
+
 }
 
+
+function convertGptTalkList2GeminiTalkList(gptTalklist) {
+    console.log("gqt对话列表为：" + gptTalklist.length);
+    let geminiTalkList = [];
+    for (let i = 0; i < gptTalklist.length; i++) {
+        let gptTalk = gptTalklist[i];
+        let geminiTalk = {
+            "role": gptTalk.role == "system" ? "model" : "user",
+            "parts": [
+                {
+                    "text": "Write the first line of a story about a magic backpack."
+                }
+            ]
+        }
+        geminiTalkList.push(geminiTalk);
+    }
+    console.log(geminiTalkList);
+    return geminiTalkList;
+}
+
+async function buildStreamResponse(msg) {
+    const stream = new ReadableStream({
+        start(controller) {
+            controller.enqueue(`data: [DONE] \n\n`);
+            controller.enqueue(`data: {
+                  "id": "chatcmpl-9yBOQH3TdLCRmVS82gnABaHQ0jb25",
+                  "object": "chat.completion.chunk",
+                  "created": 1702820230,
+                  "model": "gpt-3.5-turbo",
+                  "choices": [
+                      {
+                          "delta": {},
+                          "index": 0,
+                          "finish_reason": "stop"
+                      }
+                  ]
+                  }\n\n`);
+            controller.enqueue("data: " + convert2GptResponse(msg) + `\n\n`);
+            controller.close();
+        }
+    });
+    // 设置适当的响应头
+    const headers = new Headers();
+    headers.set('Content-Type', 'text/event-stream'); // 设置为text/event-stream
+
+    // 返回流式数据给客户端
+    return new Response(stream, {
+        status: 200,
+        statusText: 'OK',
+        headers,
+    });
+}
+
+async function buildResponse(msg) {
+    return new Response(convert2GptResponse(msg), {
+        headers: {
+            "content-type": "application/json",
+            "access-control-allow-credentials": "true",
+            "access-control-allow-headers": "Origin,Content-Type,Accept,User-Agent,Cookie,Authorization,X-Auth-Token,X-Requested-With",
+            "access-control-allow-methods": "GET,PUT,POST,DELETE,PATCH,HEAD,CONNECT,OPTIONS,TRACE",
+            "access-control-allow-origin": "*",
+            "access-control-max-age": "3628800",
+        }
+    })
+}
